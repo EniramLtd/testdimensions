b9f7e1b7da5217a94e855e1d7a5d6cdccfced04d
Antti Kaihola <antti.kaihola@eniram.fi>
2016-11-16 11:27:05 +0200

    Omit tests module when installing

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ from setuptools import setup
 setup(name='testdimensions',
       version='0.0.1.dev',
       license='FreeBSD',
+      py_modules=['testdimensions'],
       tests_require=['pytest-mock>=1.2'])
